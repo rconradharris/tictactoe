ceee6d909fe87fbb91bfddd53fe20cdc646b58df
Rick Harris <rconradharris@gmail.com>
2025-02-27 13:09:29 -0600

    README update

--- engine/mnk/winimaxer.py
+++ engine/mnk/winimaxer.py
@@ -11,8 +11,19 @@ class Winimaxer(Engine):
     Winimaxer applies the naive minimax algorithm to search the game tree for
     wins and draws.
 
-    If the `max_plies` is large enough, the engine will play optimally but
-    extremely slowly.
+    Winimax doesn't use a heuristic, it strictly look for wins and draws. That
+    means, if a the game doesn't terminate within its search horizon, it
+    considers all moves to be effectively draws.
+
+    This often leads to it choosing suboptimal moves.
+
+    An improvement would be to use a heuristic to select more promising moves,
+    like picking cells with more neighbors and picking cells that increase the
+    `run_count`.
+
+    All that said, if you make `max_plies` large enough so that the search
+    horizon is the entire game tree, Winimax is guaranteed to play optimally
+    (and very slowly).
     """
     DEFAULT_PLIES = 7
 
